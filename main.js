c243f27dde5e73da8f4cf3c1d144680ba5bef544
Daniel Harris <dh.harris183@gmail.com>
2016-05-14 00:57:28 -0700

    Added "routing"

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ const http = require('http')
 const url = require('url');
 const fs = require('fs')
 
-var checklist_path = "/Users/Keithh/Documents/Code/supersimplechecklist/templates/checklist.html"
+const tmpl_path = "/Users/Keithh/Documents/Code/supersimplechecklist/templates/"
 
 const s = http.createServer((req,res)=>{
     var user_url = url.parse(req.url).pathname
-    console.log(user_url)
-    open_file = fs.readFile(checklist_path, {encoding:"utf8"},(err,data)=>{
-        if (err) throw err;
-        res.end(data)
-    })
+    switch(user_url){
+        case "/about":
+            res.end("This is the about page")
+            break;
+        default:
+            fs.readFile(tmpl_path + "checklist.html", {encoding:"utf8"},(err,data)=>{
+                if (err) throw err;
+                res.end(data)
+            });
+    }
 
 })
 
